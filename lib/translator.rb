774d876c911100555900d051646f2ebd33f64081
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-19 13:26:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,11 @@ end
 
 def get_japanese_emoticon(file_path,emoticon)
   emoticons = load_library(file_path)
+  result = library["get_emoticon"][emoticon]
+  if result 
+    result
+  else
+    "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
