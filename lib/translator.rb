6458fd9530becb6dddb203aa2ac9952510ac3acb
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-19 13:29:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_japanese_emoticon(file_path,emoticon)
 end
 end 
 
-def get_english_meaning
+def get_english_meaning(file_path,emoticon)
 emoticons = load_library(file_path)
 result = emoticons["get_meaning"][meaning]
 if result
